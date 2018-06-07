340c83d8a9e82b96f668b4c078e2a1ca45774c50
samilawson <samilawson17@gmail.com>
2018-06-07 16:12:44 -0500

    create tutorial project

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.addSong = this.addSong.bind(this)
+    this.updateSong = this.updateSong.bind(this)
+    this.state = {
+      songs: { },
+      authenticated: false
+    };
+  }
+  render() {
+    return (
+      <div style={{maxWidth: "1160px", margin: "0 auto"}}>
+        <BrowserRouter>
+          <div>
+            <Header authenticated={this.state.authenticated} />
+            <div className="main-content" style={{padding: "1em"}}>
+              <div className="workspace">
+                <Route exact path="/songs" render={(props) => {
+                  return (
+                    <SongList songs={this.state.songs} />
+                  )
+                }} />
+                <Route path="/songs/:songId" render={(props) => {
+                  const song = this.state.songs[props.match.params.songId];
+                  return (
+                    song ? <ChordEditor song={song} updateSong={this.updateSong} /> : <h1>Song not found</h1>
+                  )
+                }} />
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Popover, PopoverInteractionKind, Position } from '@blueprintjs/core'
+//import NewSongForm from './NewSongForm'
+
+class Header extends Component {
+    render() {
+        return (
+            <nav className="pt-navbar">
+                <div className="pt-navbar-group pt-align-left">
+        )
+    }
+}
